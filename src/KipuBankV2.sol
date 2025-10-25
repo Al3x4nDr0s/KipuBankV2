6229562ea2b40dd4a4ea11c4821c0c40f88efff7
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2025-10-25 19:08:21 -0300

    Update KipuBankV2.sol

--- src/KipuBankV2.sol
+++ src/KipuBankV2.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
-//import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Interfaz de Chainlink
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Interfaz de Chainlink
 
 /// @title KipuBankV2
 /// @author Alejandro Cardenas
